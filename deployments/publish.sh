3667966f8d4b8cf092c08f21a0cb7e3bda8c2a64
Thiago Macedo <thiago@internetbudi.com.br>
2022-08-07 21:19:31 -0500

    fixing publish script build step

--- deployments/publish.sh
+++ deployments/publish.sh
@@ -3,22 +3,28 @@
 HOST=${1}
 FOLDER=${2}
 COMMAND=${3:-publish}
+BASEDIR=`realpath $(dirname "$0")/../`
 
 echo "HOST=$HOST COMMAND=$COMMAND"
 
 if [[ $COMMAND = "publish" ]]; then
 
+    TMP_FOLDER="$FOLDER/tmp"
+    APP_FOLDER="$FOLDER/catalog"
+
+    cd $BASEDIR
     git archive -o ../catalog.zip HEAD
     scp ../catalog.zip "$HOST":~
     ssh "$HOST" "
-        unzip -q -u -o catalog.zip -d $FOLDER && \
-        cd $FOLDER/deployments/ && \
+        unzip -q -u -o ~/catalog.zip -d $APP_FOLDER && \
+        cd $APP_FOLDER/deployments/ && \
+        export TMPDIR=$TMP_FOLDER TMP=$TMP_FOLDER TEMP=$TMP_FOLDER; \
+        mkdir -p \$TMP && \
         docker-compose build && \
-        docker-compose -f docker-compose.yml -f docker-compose-production.yml up -d && \
+        docker-compose up -d && \
         docker-compose exec -T web python manage.py migrate --noinput && \
         docker-compose exec -T web deployments/django/entrypoint.sh build && \
-        docker-compose exec -T web python manage.py collectstatic --noinput && \
-        "
+        docker-compose exec -T web python manage.py collectstatic --noinput"
 
 else
     echo "$COMMAND command not implemented"
