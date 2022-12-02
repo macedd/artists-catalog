21f1efc1eeea93ffbfb6891657859f6627accfcc
Thiago Macedo <thiago@internetbudi.com.br>
2022-12-01 22:43:18 -0500

    fixes router to not use base path

--- apps/theme/urls.py
+++ apps/theme/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    # path('', views.vuejs, {'resource': ''}, name='homepage'),
     path('vue', views.vuejs, {'resource': ''}, name='vuejs-test'),
     path('<path:resource>', views.vuejs, name='vuejs-app'),
     # re_path(r'^.*/?$', views.vuejs, name='vuejs')
--- frontends/src/router/index.ts
+++ frontends/src/router/index.ts
@@ -4,7 +4,7 @@ import HomeView from "../views/HomeView.vue";
 import HomePlaceholder from '../views/HomePlaceholder.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   routes: [
     {
       path: "/",
